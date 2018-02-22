df326fe826ddd94e47e4373dfad83f7d6f38f8b0
Jian Kang <jkang51@asu.edu>
2018-02-22 15:15:14 -0700

    Add files via upload

--- TextFormatter.java
+++ TextFormatter.java
@@ -1,29 +1,31 @@
 import java.io.*;
 import java.util.*;
 
-
 public class TextFormatter {
     private static final int COL_WIDTH = 80;
+    private Stats stats = new Stats();
 
-    public static ArrayList<String> format(String PATH, String direction) {
+    public ArrayList<String> format(String PATH, String direction) {
         ArrayList<String> result = new ArrayList<>();
-        if (direction == "left") {
-            result = leftJustify(PATH);
+        if (direction.equals("left")) {
+            result = leftJustify(PATH, stats);
         }
-        if (direction == "right") {
-            result = rightJustify(PATH);
+        if (direction.equals("right")) {
+            result = rightJustify(PATH, stats);
         }
         return result;
     }
 
-    public static ArrayList<String> leftJustify(String PATH) {
+    private ArrayList<String> leftJustify(String PATH, Stats stats) {
         // initialization
         ArrayList<String> lines = new ArrayList<>();
+        int free_space = COL_WIDTH;
+        StringBuffer sb = new StringBuffer();
+
         int word_cnt = 0;
         int line_cnt = 0;
         int empty_line = 0;
-        int free_space = COL_WIDTH;
-        StringBuffer sb = new StringBuffer();
+        int total_length = 0;
 
         try {
             // open text file
@@ -35,11 +37,9 @@ public class TextFormatter {
                 // split line into words
                 if (line.isEmpty()) {
                     empty_line++;
+                    continue;
                 }
                 String[] words = line.split(" ");
-
-                // stats for analysis
-                line_cnt++;
                 word_cnt += words.length;
 
                 // wrap text
@@ -54,13 +54,11 @@ public class TextFormatter {
                         }
                         sb.append(word);
                         free_space -= word.length();
-                    } else if (word.length() == free_space) {
-                        sb.append(word);
-                        lines.add(sb.toString());
-                        sb = new StringBuffer();
-                        free_space = COL_WIDTH;
                     } else {
+                        total_length += sb.length();
+                        line_cnt++;
                         lines.add(sb.toString());
+
                         sb = new StringBuffer();
                         free_space = COL_WIDTH;
                         sb.append(word);
@@ -68,7 +66,17 @@ public class TextFormatter {
                     }
                 }
             }
+
+            // wrap last line
+            total_length += sb.length();
+            line_cnt++;
+            lines.add(sb.toString());
+
+            // close file
             line_reader.close();
+
+            // update stats
+            stats.update(line_cnt, word_cnt, empty_line, total_length);
         } catch (IOException e) {
             System.err.println(e);
         }
@@ -76,14 +84,16 @@ public class TextFormatter {
     }
 
 
-    public static ArrayList<String> rightJustify(String PATH) {
+    private ArrayList<String> rightJustify(String PATH, Stats stats) {
         // initialization
         ArrayList<String> lines = new ArrayList<>();
+        int free_space = COL_WIDTH;
+        StringBuffer sb = new StringBuffer();
+
         int word_cnt = 0;
         int line_cnt = 0;
         int empty_line = 0;
-        int free_space = COL_WIDTH;
-        StringBuffer sb = new StringBuffer();
+        int total_length = 0;
 
         try {
             // open text file
@@ -95,11 +105,9 @@ public class TextFormatter {
                 // split line into words
                 if (line.isEmpty()) {
                     empty_line++;
+                    continue;
                 }
                 String[] words = line.split(" ");
-
-                // stats for analysis
-                line_cnt++;
                 word_cnt += words.length;
 
                 // wrap text
@@ -107,25 +115,23 @@ public class TextFormatter {
                     if (word == null || word.length() == 0) {
                         continue;
                     }
-                    if (word.length() < free_space - 1) {
+                    if (word.length() < free_space) {
                         if (sb.length() != 0) {
                             sb.append(" ");
                             free_space -= 1;
                         }
                         sb.append(word);
                         free_space -= word.length();
-                    } else if (word.length() == free_space) {
-                        sb.append(word);
-                        lines.add(sb.toString());
-                        sb = new StringBuffer();
-                        free_space = COL_WIDTH;
                     } else {
+                        total_length += sb.length();
+                        line_cnt++;
                         sb = sb.reverse();
                         for (int i = 0; i < free_space; i++) {
                             sb.append(" ");
                         }
                         sb = sb.reverse();
                         lines.add(sb.toString());
+
                         sb = new StringBuffer();
                         free_space = COL_WIDTH;
                         sb.append(word);
@@ -133,11 +139,30 @@ public class TextFormatter {
                     }
                 }
             }
+
+            // wrap last line
+            total_length += sb.length();
+            line_cnt++;
+            sb = sb.reverse();
+            for (int i = 0; i < free_space; i++) {
+                sb.append(" ");
+            }
+            sb = sb.reverse();
+            lines.add(sb.toString());
+
+            // close file
             line_reader.close();
+
+            // update stats
+            stats.update(line_cnt, word_cnt, empty_line, total_length);
         } catch (IOException e) {
             System.err.println(e);
         }
         return lines;
+    }
+
+    public Stats getStats() {
+        return this.stats;
     }
 
 
@@ -146,14 +171,31 @@ public class TextFormatter {
         TextFormatter TF = new TextFormatter();
         String PATH = "E:\\java\\TextFormatter\\src\\test.txt";
 
-        ArrayList<String> left = TF.leftJustify(PATH);
-        ArrayList<String> right = TF.rightJustify(PATH);
+        ArrayList<String> left = TF.format(PATH, "left");
+        Stats left_stats = TF.getStats();
+        ArrayList<String> right = TF.format(PATH, "right");
+        Stats right_stats = TF.getStats();
 
         for (String line: left) {
             System.out.println(line);
         }
+        System.out.println("#### LEFT JUSTIFY ANALYSIS ####");
+        System.out.println(left_stats.words_processed());
+        System.out.println(left_stats.lines_processed());
+        System.out.println(left_stats.empty_liens());
+        System.out.println(left_stats.avg_words_per_line());
+        System.out.println(left_stats.avg_line_length());
+
+        System.out.println();
+
         for (String line: right) {
             System.out.println(line);
         }
+        System.out.println("#### RIGHT JUSTIFY ANALYSIS ####");
+        System.out.println(right_stats.words_processed());
+        System.out.println(right_stats.lines_processed());
+        System.out.println(right_stats.empty_liens());
+        System.out.println(right_stats.avg_words_per_line());
+        System.out.println(right_stats.avg_line_length());
     }
 }
